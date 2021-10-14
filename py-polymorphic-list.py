0968efd055c83b9f84cf84e4decb7d7e85f887a2
keshprad <32313895+keshprad@users.noreply.github.com>
2021-10-14 08:12:27 -0400

    Create a polymorphic append method

--- py-polymorphic-list.py
+++ py-polymorphic-list.py
@@ -4,16 +4,20 @@ T = TypeVar("T")
 
 
 class List(Generic[T]):
+    # I'll just raise NotImplementedErrors to make List like an interface
     def __init__(self):
         raise NotImplementedError()
 
     def insert(self, element):
         raise NotImplementedError()
 
-    def append(self, element):
+    def append(self, element: T) -> 'NonEmptyList':
         raise NotImplementedError()
 
     def find(self, element):
+        raise NotImplementedError()
+
+    def __str__() -> str:
         raise NotImplementedError()
 
 
@@ -22,8 +26,15 @@ class NonEmptyList(List[T]):
         self.data: T = data
         self.next: List[T] = next
 
+    def append(self, element: T) -> 'NonEmptyList':
+        self.next = self.next.append(element)
+        return self
+
 
 class EmptyList(List[T]):
-    def __init__(self, ):
-        self.data: T = None
-        self.next: List[T] = None
+    def __init__(self):
+        # Exists to prevent super constructor from executing
+        pass
+
+    def append(self, element: T) -> NonEmptyList:
+        return NonEmptyList(element, self)
