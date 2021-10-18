58e60d04f3275b9aca269de92cdc2b9226ed87c0
keshprad <32313895+keshprad@users.noreply.github.com>
2021-10-18 19:18:22 -0400

    (docs): use docusaurus to set up template

---
+++ docs/docusaurus.config.js
@@ -0,0 +1,94 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'py_polymorphic_list',
+  tagline: 'Dinosaurs are cool',
+  url: 'https://keshprad.github.io/',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'https://keshprad.github.io/my-portfolio/favicon.ico',
+  organizationName: 'keshprad', // Usually your GitHub org/user name.
+  projectName: 'py_polymorphic_list', // Usually your repo name.
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/keshprad/py_polymorphic_list/edit/main/docs/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      colorMode: {
+        defaultMode: 'dark',
+      },
+      navbar: {
+        title: 'py_polymorphic_list',
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Docs',
+          },
+          {
+            href: 'https://github.com/keshprad/py_polymorphic_list',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+              {
+                label: 'Tutorial',
+                to: '/docs/usage',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/keshprad/py_polymorphic_list',
+              },
+            ],
+          },
+        ],
+        copyright: `<a href="https://keshprad.ml" target="_blank">keshprad</a> — ${new Date().getFullYear()}. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
---
+++ docs/src/pages/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import { Redirect } from '@docusaurus/router';
+
+export default function Home() {
+  const { siteConfig } = useDocusaurusContext();
+  return <Redirect to="/docs/intro" />;
+}
